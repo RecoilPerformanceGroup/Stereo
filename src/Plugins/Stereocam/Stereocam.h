2dbf5ef5afd9dca78c49c7ea13f0f4453a3391a8
Johan Bichel Lindegaard <johan@johan.cc>
2014-01-09 19:47:27 -0500

    StereoCam functionality added. Not compiling.

--- src/Plugins/Stereocam/Stereocam.h
+++ src/Plugins/Stereocam/Stereocam.h
@@ -1,9 +1,13 @@
 #pragma once
 #import <ofxCocoaPlugins/Plugin.h>
-
+#import <ofxStereoCamera.h>
 
 @interface Stereocam : ofPlugin {
 
+    ofxStereoCamera<ofEasyCam> cam;
+    ofBoxPrimitive myBox;
+    
+    
 }
 
 @end
--- src/Plugins/Stereocam/Stereocam.mm
+++ src/Plugins/Stereocam/Stereocam.mm
@@ -18,6 +18,15 @@
 
 
 -(void)setup{
+ 
+    cam.setup(ofGetWidth(), ofGetHeight());
+    cam.setScale(1, -1, 1);
+    
+    cam.setPhysicalFocusDistance(120);
+    
+    cam.setFocusDistance(50);
+    cam.setNearClip(0.1);
+    
     
 }
 
@@ -28,11 +37,34 @@
 
 -(void)update:(NSDictionary *)drawingInformation{
     
+    
+    cam.setFocusDistance(cam.getGlobalPosition().length());
+    
+    cam.update();
+    
+    cam.beginLeft();
+    [self drawScene];
+    cam.endLeft();
+    
+    cam.beginRight();
+    [self drawScene];
+    cam.endRight();
+    
+    
 }
 
 //
 //----------------
 //
+
+
+-(void)drawScene{
+    
+    myBox.set(100);
+    myBox.draw();
+    
+    
+}
 
 -(void)draw:(NSDictionary *)drawingInformation{
     
@@ -40,7 +72,7 @@
     
     ofFill();
     
-    ofSetColor(255, 255, 255,255);
+    ofSetColor(255, 0, 0,255);
     
     ApplySurfaceForProjector(@"Floor",0); {
         
@@ -48,6 +80,11 @@
         
         glTranslated(0, 0, 0.01);
         ofRect(0,0,1*aspect,1);
+        
+        
+        
+        
+        
         
     } PopSurfaceForProjector();
    
